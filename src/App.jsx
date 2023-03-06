1acd1ff99176d7b49b03e461211f9a8f7821d498
DmytroB <dmitrijbaraban@gmail.com>
2023-03-07 01:02:47 +0200

    timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,70 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import userData from './userData.js';
-import User from './User';
-import Modal from './Modal';
-import Header from './Header';
 
 function App() {
-  const [users, setUsers] = useState(userData);
-  const [isShowModal, setIsShowModal] = useState(false);
-  const [modalUser, setModalUser] = useState(null);
-  const [filter, setFilter] = useState({
-    name: '',
-    sort: 'default',
-  });
+  const [time, setTime] = useState(0); // час в секундах
+  const [timerOn, setTimerOn] = useState(false);
+  const [savedTimes, setSavedTimes] = useState([]);
 
   useEffect(() => {
-    const nameToLowerCase = filter.name.toLowerCase();
-    const sortValue = filter.sort;
-    const newUsers = userData
-      .filter((user) => user.name.toLocaleLowerCase().includes(nameToLowerCase))
-      .sort((a, b) => (sortValue === 'desc' ? a.age - b.age : b.age - a.age));
-    setUsers(newUsers);
-  }, [filter]);
-
-  useEffect(() => {
-    if (!isShowModal) {
-      setModalUser(null);
+    let interval = null;
+    if (timerOn) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime - 1); // зменшуємо час на 1 секунду
+      }, 1000);
+    } else {
+      clearInterval(interval); // зупиняємо таймер
     }
-  }, [isShowModal]);
+    return () => clearInterval(interval);
+  }, [timerOn]);
 
-  const handleInputOnChange = (event) => {
-    const { value } = event.target;
-    const newState = { ...filter, name: value };
-    setFilter(newState);
-  };
-  const handleSortOnChange = (event) => {
-    const { value } = event.target;
-    const newState = { ...filter, sort: value };
-    setFilter(newState);
+  const startTimer = () => {
+    setTimerOn(true);
   };
 
-  const handleShowModal = (user) => {
-    setIsShowModal(true);
-    setModalUser(user);
+  const stopTimer = () => {
+    setSavedTimes([...savedTimes, time]);
+    setTimerOn(false);
   };
 
-  const handleCloseModal = () => {
-    setIsShowModal(false);
+  const resetTimer = () => {
+    setTime(0);
+    setTimerOn(false);
+    setSavedTimes([]);
+  };
+
+  // конвертуємо час у формат HH:MM:SS
+  const formatTime = (time) => {
+    const hours = Math.floor(time / 3600)
+      .toString()
+      .padStart(2, '0');
+    const minutes = Math.floor((time % 3600) / 60)
+      .toString()
+      .padStart(2, '0');
+    const seconds = Math.floor(time % 60)
+      .toString()
+      .padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
   };
 
   return (
     <div className="container">
-      {isShowModal ? (
-        <Modal handleCloseModal={handleCloseModal} user={modalUser} />
-      ) : null}
-      <Header
-        handleInputOnChange={handleInputOnChange}
-        handleSortOnChange={handleSortOnChange}
-      />
-      <div className="users-list">
-        {users.map((user) => (
-          <User user={user} handleShowModal={() => handleShowModal(user)} />
-        ))}
+      <h2>{formatTime(time)}</h2>
+      <div className="btn">
+        {!timerOn && <button onClick={startTimer}>Start</button>}
+        {timerOn && <button onClick={stopTimer}>Stop</button>}
+        <button onClick={resetTimer}>Reset</button>
       </div>
+      <div className="btn">
+        <button onClick={() => setTime(60)}>1 minute</button>
+
+        <button onClick={() => setTime(180)}>3 minute</button>
+      </div>
+      <ul>
+        {savedTimes.map((time, index) => (
+          <li key={index}>{formatTime(time)}</li>
+        ))}
+      </ul>
     </div>
   );
 }
