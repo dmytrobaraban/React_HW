7e58e9d2984942cb9093b8ac1b837c115c38c809
DmytroB <dmitrijbaraban@gmail.com>
2023-03-14 02:46:58 +0200

    change logic for btn

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [time, setTime] = useState(0);
+  const [time, setTime] = useState(0); // час в секундах
   const [timerOn, setTimer] = useState(false);
   const [savesTime, setSavesTime] = useState([]);
+  const [stopped, setStopped] = useState(false);
+  const [isReset, setIsReset] = useState(false);
 
   const soundStart = new Audio(
     'https://cdn.videvo.net/videvo_files/audio/premium/audio0086/watermarked/el_interface_button_22_hpx_preview.mp3'
@@ -20,33 +22,65 @@ function App() {
     let interval = null;
     if (timerOn) {
       interval = setInterval(() => {
-        setTime((prevTime) => prevTime - 1);
-      }, 1000);
+        setTime((prevTime) => prevTime - 0.1);
+      }, 100); // оновлюємо кожні 100 мілісекунд
     } else {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
   });
 
-  const timerStart = () => {
-    soundStart.play();
+  useEffect(() => {
+    if (timerOn && Math.round(time) === 0) {
+      setTimer(false);
+      setTime(0);
+      alert('Time is out!');
+    }
+  }, [timerOn, time]);
+
+  useEffect(() => {
+    // Отримуємо збережені дані з локального сховища
+    const savedTime = localStorage.getItem('savedTimes');
+    if (savedTime) {
+      // Парсимо рядок JSON в масив
+      setSavesTime(JSON.parse(savedTime));
+    }
+  }, []);
+
+  // Функція, яка зберігає час у локальному сховищі
+  const saveTime = (time) => {
+    // Конвертуємо масив у рядок JSON та зберігаємо його в локальному сховищі
+    localStorage.setItem('savedTimes', JSON.stringify([...savesTime, time]));
+  };
+
+  const timerStart = (time) => {
     setTimer(true);
+    soundStart.play();
+  };
+
+  const timerContinue = () => {
+    setTimer(true);
+    setStopped(false);
+    soundStart.play();
   };
 
   const timerStop = () => {
-    soundStop.play();
-    setSavesTime([...savesTime, time]);
     setTimer(false);
+    setSavesTime([...savesTime, time]);
+    saveTime();
+    setStopped(true);
+    soundStop.play();
   };
 
   const resetTimer = () => {
-    soundReset.play();
-    timerStop();
+    setTimer(false);
     setTime(0);
     setSavesTime([]);
+    setIsReset(true);
+    soundReset.play();
   };
 
-  // конвертуємо час у формат HH:MM:SS
+  // конвертуємо час у формат HH:MM:SS:MSMS
   const formatTime = (time) => {
     const hours = Math.floor(time / 3600)
       .toString()
@@ -57,20 +91,28 @@ function App() {
     const seconds = Math.floor(time % 60)
       .toString()
       .padStart(2, '0');
-    return `${hours}:${minutes}:${seconds}`;
+    const milliseconds = Math.floor((time % 1) * 1000)
+      .toString()
+      .padStart(4, '0');
+    return `${hours}:${minutes}:${seconds}:${milliseconds}`;
   };
 
   return (
     <div className="container">
       <h1>{formatTime(time)}</h1>
       <div className="btn">
-        {!timerOn && <button onClick={timerStart}>start</button>}
-        {timerOn && <button onClick={timerStop}>stop</button>}
-        <button onClick={resetTimer}>reset</button>
+        {(!timerOn && !stopped) || isReset ? (
+          <button onClick={timerStart}>Start</button>
+        ) : (
+          <button onClick={timerContinue}>Continue</button>
+        )}
+        <button onClick={timerStop}>Stop</button>
+        <button onClick={resetTimer}>Reset</button>
       </div>
       <div className="btn">
-        <button onClick={() => setTime(60)}>1 minute</button>
+        <button onClick={() => setTime(2)}>1 minute</button>
         <button onClick={() => setTime(180)}>3 minute</button>
+        <button onClick={() => setTime(300)}>5 minute</button>
       </div>
       <ul>
         {savesTime.map((time, index) => (
